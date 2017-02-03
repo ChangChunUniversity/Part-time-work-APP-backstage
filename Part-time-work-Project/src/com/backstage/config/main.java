d65df98bc6d2879e11dda56cec47ce950c1b7639
laujangit@gmail.com <1urmygithub>
2017-02-03 22:45:00 +0800

    Initial commit

---
+++ Part-time-work-Project/src/com/backstage/config/main.java
@@ -0,0 +1,15 @@
+package com.backstage.config;
+
+import java.io.IOException;
+/**
+ * Created by lauya on 2017/2/3.
+ * this is a test document
+ */
+public class main {
+
+    //test
+    private int test(){
+        return 2;
+    }
+}
+
---
+++ Part-time-work-Project/src/com/backstage/model/main.java
@@ -0,0 +1,13 @@
+package com.backstage.model;
+
+import java.io.IOException;
+/**
+ * Created by lauya on 2017/2/3.
+ * test document
+ */
+public class main {
+    //test
+    private String test(){
+        return "test";
+    }
+}
