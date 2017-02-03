c9b1bc7dc343ef814f6cfb854d3476f4d00c19ce
laujangit@gmail.com <1urmygithub>
2017-02-03 22:45:00 +0800

    Initial commit

---
+++ Part-time-work-Project/src/com/backstage/controller/register.java
@@ -0,0 +1,16 @@
+package com.backstage.controller;
+
+import java.io.IOException;
+
+/**
+ * Created by lauya on 2017/2/3.
+ */
+public class register extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
