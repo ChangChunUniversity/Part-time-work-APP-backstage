cf632a59c751eaf1b4bf3d9ee7d267415ae04f2d
laujangit@gmail.com <1urmygithub>
2017-02-04 21:14:16 +0800

    initial commit

--- Part-time-work-Project/src/com/backstage/model/register/format/CheckFormat.java
+++ Part-time-work-Project/src/com/backstage/model/register/format/CheckFormat.java
@@ -3,9 +3,9 @@ package com.backstage.model.register.format;
 /**
  * Created by lauya on 2017/2/4.
  */
-public class CheckFormat {
+public class CheckFormat{
     public static Boolean isCorrectNickName(String nickName){
-        if(nickName.length()>=24||CheckFormat.isSQL(nickName)){
+        if(com.backstage.model.userinfo.format.CheckFormat.isCorrectNickName(nickName)||CheckFormat.isSQL(nickName)){
             return false;
         }
         return true;
