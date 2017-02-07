d1e307e6d101e4094ecbe5501300fa0cc2dfc4f7
laujangit@gmail.com <1urmygithub>
2017-02-07 15:29:55 +0800

    no commit

--- Part-time-work-Project/src/com/backstage/controller/register.java
+++ Part-time-work-Project/src/com/backstage/controller/register.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import com.google.gson.Gson;
 
-import com.backstage.model.register.format.BaseInputJsonStrute;
+import com.backstage.model.register.format.*;
 import com.backstage.model.register.database.DataBase;
 import com.backstage.model.gson.Error;
 
@@ -19,8 +19,8 @@ public class register extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         String str = "{\"nickname\":\"testname\",\"password\":123456pwd,\"sex\":1,\"phone\":\"13634187645\",\"email\":\"test@qq.test.com\"}";
-        Gson json = new Gson();
-        BaseInputJsonStrute recvData = json.fromJson(str, BaseInputJsonStrute.class);
+        Gson inputJson = new Gson();
+        BaseInputJsonStrute recvData = inputJson.fromJson(str, BaseInputJsonStrute.class);
 
         String nickName;
         String email;
@@ -39,11 +39,18 @@ public class register extends javax.servlet.http.HttpServlet {
         } catch (IllegalArgumentException e) {
             out.write(Error.ReturnError(e));
         }
+
         //检验输出值合法性
         //构建JSON
         //输出JSON
         //#定义错误码
         //#定义错误码解释
+
+        Gson outputJson = new Gson();
+        BaseOutputJsonStrute sendData=new BaseOutputJsonStrute();
+        sendData.status=0;
+        sendData.token="";
+        out.write(outputJson.toJson(sendData));
 
 
         //out.write("Error");
--- Part-time-work-Project/src/com/backstage/model/register/format/BaseOutputJsonStrute.java
+++ Part-time-work-Project/src/com/backstage/model/register/format/BaseOutputJsonStrute.java
@@ -4,4 +4,6 @@ package com.backstage.model.register.format;
  * Created by lauya on 2017/2/7.
  */
 public class BaseOutputJsonStrute {
+    public int status;
+    public String token;
 }
