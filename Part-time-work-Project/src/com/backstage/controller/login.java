43e697da12a2cda05affd543efbb4f4c3cb9f747
laujangit@gmail.com <1urmygithub>
2017-02-08 20:06:56 +0800

    add login moudle

--- Part-time-work-Project/src/com/backstage/controller/login.java
+++ Part-time-work-Project/src/com/backstage/controller/login.java
@@ -4,6 +4,9 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import com.backstage.view.login.BaseInputJSON;
 import com.google.gson.Gson;
 
 /**
@@ -16,7 +19,10 @@ public class login extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String str="{\"username\":\"xxxxxx\",\"password\":\"xxxxxx\",\"timestamp\":1486551703,\"DeviceID\":“352087074301038”}";
-
+        String str="{\"username\":\"xxxxxx\",\"password\":\"xxxxxx\",\"timestamp\":1486551703,\"deviceid\":\"352087074301038\"}";
+        Gson inputJson=new Gson();
+        BaseInputJSON recvData = inputJson.fromJson(str, BaseInputJSON.class);
+        PrintWriter out=response.getWriter();
+        out.write(recvData.password);
     }
 }
---
+++ Part-time-work-Project/src/com/backstage/view/login/BaseInputJSON.java
@@ -0,0 +1,11 @@
+package com.backstage.view.login;
+
+/**
+ * Created by jangitlau on 2017/2/8.
+ */
+public class BaseInputJSON {
+    public String username;
+    public String password;
+    public int timestamp;
+    public String deviceid;
+}
